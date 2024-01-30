02fc9c05b23f777a7202e64412673d65cf21fbf4
Mathis Santos <mail@mathissantos.com>
2024-01-30 08:56:39 +0100

    created styled component for Spotlight

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import ArtPiecePreview from "@/components/ArtPiecePreview";
 import useSWR from "swr";
 import Spotlight from "@/components/Spotlight";
-import { getRandomArtPiece } from "@/utils/getRandomArtPiece";
+import { getRandomImage } from "@/utils/getRandomArtPiece";
 
 export default function HomePage() {
   const { data, isLoading, error } = useSWR(
@@ -10,11 +10,11 @@ export default function HomePage() {
   if (!isLoading) {
     <p>Loading...</p>;
   }
-  if (error) {
+  if (!error) {
     <p>404 error</p>;
   }
 
-  const spotlightPiece = data ? getRandomArtPiece(data) : null;
+  const spotlightPiece = data ? getRandomImage(data) : null;
 
   return (
     <div>
