4914b02646fb6493dd7abc3bb668ada88779f9ef
YavorLalev <yavorlalev@abv.bg>
2024-02-03 16:06:34 +0100

    Add ArtPieceDetails component

---
+++ components/ArtPieceDetails/index.js
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Button from "../Button";
+import Link from "next/link";
+import styled from "styled-components";
+
+const StyledSection = styled.section`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+export default function ArtPieceDetails({ image, title, artist, year, genre }) {
+  return (
+    <>
+      <StyledSection>
+        <h2>"{title}"</h2>
+        <h3>{artist}</h3>
+        <Image src={image} alt={title} width={400} height={400} />
+        <p>
+          {genre}
+          {","} {year}
+        </p>
+
+        <Link
+          style={{ textDecoration: "none", color: "white" }}
+          href="/art-pieces"
+        >
+          <Button $backgroundcolor="salmon">Back to Index</Button>
+        </Link>
+      </StyledSection>
+    </>
+  );
+}
