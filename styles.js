bcc0c8c3c910e15d08ce773f0199d4835590ab18
YavorLalev <yavorlalev@abv.bg>
2024-02-03 16:12:02 +0100

    Updated styles.js

--- styles.js
+++ styles.js
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from "styled-components";
+import { Work_Sans } from "next/font/google";
 
+const workSans = Work_Sans({ subsets: ["latin"] });
 export default createGlobalStyle`
   *,
   *::before,
@@ -7,8 +9,18 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
+  :root {
+    --primary-color:#FA8072;
+    --secondary-color: #FFA07A;
+    --tertiary-color: #A5A5A5;
+  }
+
   body {
     margin: 0;
-    font-family: system-ui;
+    font-family: ${workSans.style.fontFamily};
   }
+
+  
+
+   
 `;
