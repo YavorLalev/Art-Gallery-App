55fbbff09cb1e5fc0b015637f9ab1d7e4dcb1ece
YavorLalev <yavorlalev@abv.bg>
2024-02-03 16:10:33 +0100

    Updated Spotlight component

--- components/Spotlight/index.js
+++ components/Spotlight/index.js
@@ -1,11 +1,28 @@
 import Image from "next/image";
-import { StyledDiv, StyledH3 } from "@/components/Spotlight/styles";
+import styled from "styled-components";
 
-export default function SpotlightPage({ image, artist, name }) {
+export const StyledDiv = styled.div`
+  position: relative;
+  width: 400px;
+  height: 400px;
+`;
+
+export const StyledH3 = styled.h3`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  color: white;
+  background-color: rgba(0, 0, 0, 0.4);
+  padding: 10px;
+  margin: 0;
+`;
+
+export default function Spotlight({ image, artist, title }) {
   return (
     <StyledDiv>
-      <Image src={image} alt={name} layout="fill" objectFit="cover"></Image>
       <StyledH3>{artist}</StyledH3>
+      <Image src={image} alt={title} width={400} height={400} />
     </StyledDiv>
   );
 }
